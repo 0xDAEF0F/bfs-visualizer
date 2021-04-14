9a4b0ab1005789678a64fbe3952ec6e553fb6c59
daemon <0xDAEF0F@proton.me>
2021-04-13 19:41:12 -0600

    hamburgerMenu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import logo from "../assets/logo.svg"
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { ReactComponent as Logo } from "../assets/logo.svg";
 
 function Header({ location }) {
 
@@ -19,19 +20,38 @@ function Header({ location }) {
     }
 
     return (
-        <div className="header">
-            <div className={'logo-container'}>
-                <img src={logo} alt="logo" />
-                <Link to="/" className="logo-name">AlgoSolver</Link>
-            </div>
-            <div className="sections">
-                <Link to={routes[0]}>{names[0]}</Link>
-                <Link to={routes[1]}>{names[1]}</Link>
-            </div>
-            <div className="resources">
-                <Link to="/">Learning Resources</Link>
-            </div>
-        </div>
+        // <div className="header">
+        //     <div className={'logo-container'}>
+        //         <img src={logo} alt="logo" />
+        //         <Link to="/" className="logo-name">AlgoSolver</Link>
+        //     </div>
+        //     <div className="sections">
+        //         <Link to={routes[0]}>{names[0]}</Link>
+        //         <Link to={routes[1]}>{names[1]}</Link>
+        //     </div>
+        //     <div className="resources">
+        //         <Link to="/">Learning Resources</Link>
+        //     </div>
+        // </div>
+        <Navbar className='navbar-dark' expand='lg'>
+            <Navbar.Brand href='/'>
+                <Logo
+                    alt="logo"
+                />
+                algosolver
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse>
+                <Nav className='mr-auto'>
+                    <Nav.Link href={routes[0]}>{names[0]}</Nav.Link>
+                    <Nav.Link href={routes[1]}>{names[1]}</Nav.Link>
+                </Nav>
+                <NavDropdown.Divider />
+                <Nav>
+                    <Nav.Link>Learning Resources</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
